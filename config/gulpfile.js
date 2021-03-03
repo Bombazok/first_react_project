d2717aca1573f4b54db725922c52891096227f74
Bombazok <sergeipiskarev@yandex.ru>
2021-03-03 16:15:09 +0300

    better setup for react-sripts & gulp sync

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -2,15 +2,16 @@ const browserSync = require("browser-sync").create(),
 	gulp = require("gulp"),
 	sass = require("gulp-sass"),
 	cleanCSS = require("gulp-clean-css"),
-	autoprefixer = require("gulp-autoprefixer"),
-	terser = require("gulp-terser"),
-	pug = require("gulp-pug");
+	autoprefixer = require("gulp-autoprefixer");
 
 gulp.task(
 	"server",
 	gulp.series(function () {
 		browserSync.init({
-			proxy: "http://localhost:3000",
+			proxy: {
+				target: "http://localhost:3000",
+				ws: true,
+			},
 		});
 	})
 );
@@ -29,37 +30,10 @@ gulp.task(
 );
 
 gulp.task(
-	"js",
-	gulp.series(function () {
-		return gulp
-			.src("../src/script/*.js")
-			.pipe(terser())
-			.pipe(gulp.dest("../public/script/"));
-	})
-);
-
-gulp.task(
-	"pug",
-	gulp.series(function () {
-		return gulp
-			.src("../src/pug/pages/*.pug")
-			.pipe(pug())
-			.pipe(gulp.dest("../public/pages/"));
-	})
-);
-
-gulp.task(
 	"watch",
 	gulp.series(function () {
 		gulp.watch("../src/scss/**/*.scss", gulp.series("sass"));
-		gulp.watch("../src/script/**/*.js", gulp.series("js")).on(
-			"change",
-			browserSync.reload
-		);
-		gulp.watch("../src/pug/**/*.pug", gulp.series("pug")).on(
-			"change",
-			browserSync.reload
-		);
+		gulp.watch("../src/**/*.js").on("change", browserSync.reload);
 	})
 );
 
