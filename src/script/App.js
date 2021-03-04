08dea1181df6719f010639923d5e3793be880d9f
Bombazok <sergeipiskarev@yandex.ru>
2021-03-05 00:22:27 +0300

    props & map

--- src/script/App.js
+++ src/script/App.js
@@ -1,5 +1,21 @@
+function Sample(props) {
+	return (
+		<div className={`Sample_${props.name}`}>
+			<h1>Sample {props.value}</h1>
+		</div>
+	);
+}
+
 function App() {
-	return <div className="App"></div>;
+	let elements = ["1", "2", "3"];
+	return (
+		<div className="App">
+			<h1>Привет</h1>
+			{elements.map((value, index) => (
+				<Sample name={`check_${index + 1}`} value={value} />
+			))}
+		</div>
+	);
 }
 
 export default App;
